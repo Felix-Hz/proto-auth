5bd5290c4d75494d0617355cb2df6d165a63ef11
Félix Hernández Vieyra <71148989+Felix-Hz@users.noreply.github.com>
2024-01-25 14:26:59 +0100

    add: user email

--- protoauth/settings.py
+++ protoauth/settings.py
@@ -24,8 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-%*_!e0f-ogh32bc2mh@p*55roml)6&a^e22+6ra4p))%(a5_6&"
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
