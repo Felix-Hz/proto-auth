12a4ec796fb576753df280071a4ff4f0ba4e65f4
Félix Hernández Vieyra <71148989+Felix-Hz@users.noreply.github.com>
2024-01-30 23:41:06 +0100

    fix: routing

--- auth_api/urls.py
+++ auth_api/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
-from . import views
+from . import views_auth, views_posts
 
 urlpatterns = [
-    path("", views.api_home),
-    path("register/", views.RegistrationAPIView.as_view(), name="register"),
-    path("login/", views.LoginAPIView.as_view(), name="login"),
-    path("logout/", views.LogoutAPIView.as_view(), name="logout"),
-    path("publish/", views.PublishPostAPIView.as_view(), name="publish_post"),
-    path("react/", views.ReactPostAPIView.as_view(), name="react_post"),
-    path("comment/", views.AddCommentAPIView.as_view(), name="comment_post"),
+    # Dummy.
+    path("", views_auth.api_home),
+    # Authenticate, and authorize.
+    path("register/", views_auth.RegistrationAPIView.as_view(), name="register"),
+    path("login/", views_auth.LoginAPIView.as_view(), name="login"),
+    path("logout/", views_auth.LogoutAPIView.as_view(), name="logout"),
+    # Posts, and interactions.
+    path("publish/", views_posts.PublishPostAPIView.as_view(), name="publish_post"),
+    path("react/", views_posts.ReactPostAPIView.as_view(), name="react_post"),
+    path("comment/", views_posts.AddCommentAPIView.as_view(), name="comment_post"),
 ]
